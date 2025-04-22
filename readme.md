4884020c1ae788598bd23235e8a523b519b23680
Millimeter <akumakumu@gmail.com>
2025-04-23 03:15:02 +0700

    add readme and sql for users

---
+++ readme.md
@@ -0,0 +1,11 @@
+# overview
+
+# dev note(s)
+
+ain't no way i'm doing migrations—just refer to `sql/` for the database schema.
+
+```bash
+go mod tidy
+
+make build && make run
+```
---
+++ sql/user-schema.sql
@@ -0,0 +1,19 @@
+CREATE TYPE IF NOT EXISTS user_status AS ENUM ('pending', 'active', 'inactive', 'suspend');
+
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    user_uuid UUID DEFAULT gen_random_uuid () NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    phone_number VARCHAR(255) UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    status user_status NOT NULL DEFAULT 'pending',
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
+    deleted_at TIMESTAMPTZ
+);
+
+DROP TRIGGER IF EXISTS update_users_updated_at ON users;
+
+CREATE TRIGGER update_users_updated_at BEFORE
+UPDATE ON users FOR EACH ROW EXECUTE FUNCTION update_updated_at_column ();
