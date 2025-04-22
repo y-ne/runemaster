8ca0cb1353e25d15bb7a2e244cf723eaa9c2f14b
Millimeter <akumakumu@gmail.com>
2025-04-23 04:24:57 +0700

    implement Argon2 password config and partial indexes

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,6 +1,9 @@
 package model
 
 import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
@@ -16,23 +19,37 @@ const (
 )
 
 type Argon2Config struct {
-	Salt      string `json:"salt"`
-	Time      uint32 `json:"time"`
-	Memory    uint32 `json:"memory"`
-	Threads   uint8  `json:"threads"`
-	KeyLength uint32 `json:"keyLen"`
+	Salt      string `json:"salt" db:"salt"`
+	Time      uint32 `json:"time" db:"time"`
+	Memory    uint32 `json:"memory" db:"memory"`
+	Threads   uint8  `json:"threads" db:"threads"`
+	KeyLength uint32 `json:"keyLen" db:"keyLen"`
+}
+
+func (a Argon2Config) Value() (driver.Value, error) {
+	return json.Marshal(a)
+}
+
+func (a *Argon2Config) Scan(value any) error {
+	bytes, ok := value.([]byte)
+
+	if !ok {
+		return fmt.Errorf("failed to convert password_config to bytes")
+	}
+
+	return json.Unmarshal(bytes, a)
 }
 
 type User struct {
-	ID             int64        `json:"id"`
-	UUID           uuid.UUID    `json:"uuid"`
-	Username       string       `json:"username"`
-	Email          string       `json:"email"`
-	PhoneNumber    string       `json:"phone_number,omitempty"`
-	PasswordHash   string       `json:"-"`
-	PasswordConfig Argon2Config `json:"-"`
-	Status         UserStatus   `json:"status"`
-	CreatedAt      time.Time    `json:"created_at"`
-	UpdatedAt      time.Time    `json:"updated_at"`
-	DeletedAt      *time.Time   `json:"deleted_at,omitempty"`
+	ID             int64        `json:"id" db:"id"`
+	UUID           uuid.UUID    `json:"uuid" db:"user_uuid"`
+	Username       string       `json:"username" db:"username"`
+	Email          string       `json:"email" db:"email"`
+	PhoneNumber    string       `json:"phone_number,omitempty" db:"phone_number"`
+	PasswordHash   string       `json:"-" db:"password_hash"`
+	PasswordConfig Argon2Config `json:"-" db:"password_config"`
+	Status         UserStatus   `json:"status" db:"status"`
+	CreatedAt      time.Time    `json:"created_at" db:"created_at"`
+	UpdatedAt      time.Time    `json:"updated_at" db:"updated_at"`
+	DeletedAt      *time.Time   `json:"deleted_at,omitempty" db:"deleted_at"`
 }
