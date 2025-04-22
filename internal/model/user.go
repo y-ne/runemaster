15f2147a8b6edeea002173a05e2b6829516793fc
Millimeter <akumakumu@gmail.com>
2025-04-23 03:56:06 +0700

    update for argon2

--- internal/model/user.go
+++ internal/model/user.go
@@ -15,15 +15,24 @@ const (
 	UserStatusSuspend  UserStatus = "suspend"
 )
 
+type Argon2Config struct {
+	Salt      string `json:"salt"`
+	Time      uint32 `json:"time"`
+	Memory    uint32 `json:"memory"`
+	Threads   uint8  `json:"threads"`
+	KeyLength uint32 `json:"keyLen"`
+}
+
 type User struct {
-	ID          int64      `json:"id"`
-	UUID        uuid.UUID  `json:"uuid"`
-	Username    string     `json:"username"`
-	Email       string     `json:"email"`
-	PhoneNumber string     `json:"phone_number,omitempty"`
-	Password    string     `json:"-"`
-	Status      UserStatus `json:"status"`
-	CreatedAt   time.Time  `json:"created_at"`
-	UpdatedAt   time.Time  `json:"updated_at"`
-	DeletedAt   *time.Time `json:"deleted_at,omitempty"`
+	ID             int64        `json:"id"`
+	UUID           uuid.UUID    `json:"uuid"`
+	Username       string       `json:"username"`
+	Email          string       `json:"email"`
+	PhoneNumber    string       `json:"phone_number,omitempty"`
+	PasswordHash   string       `json:"-"`
+	PasswordConfig Argon2Config `json:"-"`
+	Status         UserStatus   `json:"status"`
+	CreatedAt      time.Time    `json:"created_at"`
+	UpdatedAt      time.Time    `json:"updated_at"`
+	DeletedAt      *time.Time   `json:"deleted_at,omitempty"`
 }
