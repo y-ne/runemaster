ea5d165024ff1e5fc46e653931644ee621106e7f
Millimeter <akumakumu@gmail.com>
2025-04-23 04:14:25 +0700

    indexes

--- sql/user-schema.sql
+++ sql/user-schema.sql
@@ -18,3 +18,13 @@ DROP TRIGGER IF EXISTS update_users_updated_at ON users;
 
 CREATE TRIGGER update_users_updated_at BEFORE
 UPDATE ON users FOR EACH ROW EXECUTE FUNCTION update_updated_at_column ();
+
+CREATE INDEX IF NOT EXISTS idx_users_status ON users (status);
+
+CREATE INDEX IF NOT EXISTS idx_users_deleted_at_null ON users (deleted_at)
+WHERE
+    deleted_at IS NULL;
+
+CREATE INDEX IF NOT EXISTS idx_users_username_not_deleted ON users (username)
+WHERE
+    deleted_at IS NULL;
